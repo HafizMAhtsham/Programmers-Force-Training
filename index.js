60c587ee330c77a1686ab25736032ed5a2959a24
Hafiz Muhammad Ahtsham <hafizmuhammadahtsham@gmail.com>
2024-01-23 17:49:37 +0500

    Task of JavaScript Basic Practice is Completed

---
+++ index.js
@@ -0,0 +1,55 @@
+// let name = "Ahtsham";
+// console.log(name); 
+// //variable name should not be a reserved keyword 
+// // Variable name should be meaningful
+// // Variable name cannot start with a number (e.g 1name)
+// // Variable name cannot contain a space or hyphen(-)
+// // JavaScript is case sensitive
+// let firstName = "Muhammad";
+// console.log(firstName);
+// let lastName = "Ahtsham";
+// console.log(lastName);
+
+// let interestRate = 0.3;
+// interestRate = 1
+// console.log(interestRate);
+
+// let name = "Ahtsham";         //String Literal
+// let age = 25;                 //Number Literal
+// let isApproved = false;       //Boolean Literal
+// let firstName = undefined;
+// let selectedColor = null;
+
+//Object Literals
+// let person ={
+//     name: "Ahtsham",
+//     age: 25
+// };
+// //Dot Notation
+// person.name = "Muhammad";      //Dot notation is better choice to use
+// //Bracket Notation
+// person['name1'] = 'Ahtsham';
+
+// console.log(person.name);
+// console.log(person['name1']);
+
+// //Array Literals
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] ='green';
+// selectedColors[3] =1;
+// console.log(selectedColors);
+// console.log(selectedColors.length);
+
+
+//Performing a Task
+function greet(name, lastName){
+    console.log("Hello " + name + lastName);
+}
+
+//Calculating a value
+function square (number){
+    return number * number;
+}
+console.log(square(2));
+greet('Muhammad ', 'Ahtsham');
+
